0dd6478fa66abfe3011c8beec67a91ddf0b217c8
Zhastilek Zhalelov <zzhalelov@gmail.com>
2023-03-30 23:10:03 +0600

    2nd commit 30th March 2023

--- src/HiLo.java
+++ src/HiLo.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class HiLo {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         //Генерация случайного числа, которое будет отгадывать пользователь
-        int theNumber = (int) (Math.random()*100+1);
+        int theNumber = (int) (Math.random() * 100 + 1);
         System.out.println(theNumber);
+        int guess = 0;
+        System.out.println("Guess a number between 1 and 100:");
+        guess = scan.nextInt();
+        System.out.println("You entered " + guess + ".");
     }
 }
